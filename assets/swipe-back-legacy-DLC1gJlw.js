71e1a4a81e5a9753d7a0d54627200085f9253dd0
Himaglahi <mjobsidmagsalos>
2025-02-13 15:02:07 +0800

    Updates

--- assets/swipe-back-legacy-DLC1gJlw.js
+++ assets/swipe-back-legacy-DLC1gJlw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CbK23Oqr.js","./index3-legacy-CQdYCORi.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-A76E0BYL.js","./index3-legacy-TpBq59nh.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
