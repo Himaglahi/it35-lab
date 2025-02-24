8ab6414247a0d195d9387cd79168354ff010fbc9
Himaglahi <mjobsidmagsalos@gmail.com>
2025-02-24 08:39:15 +0800

    Updates

--- assets/keyboard2-legacy-CnuTIXVH.js
+++ assets/keyboard2-legacy-CnuTIXVH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-uKOMqGI8.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Dg1M45ln.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-PmL3e6hh.js
+++ assets/swipe-back-legacy-PmL3e6hh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-uKOMqGI8.js","./index3-legacy-DJp3klrD.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-Dg1M45ln.js","./index3-legacy-Bek-Fgw6.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
