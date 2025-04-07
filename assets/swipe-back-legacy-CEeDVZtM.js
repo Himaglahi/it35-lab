16f9b76299717b54a67eb6691a5ac7774d4e93c1
Himaglahi <mjobsidmagsalos@gmail.com>
2025-04-07 08:33:15 +0800

    Updates

--- assets/swipe-back-legacy-CEeDVZtM.js
+++ assets/swipe-back-legacy-CEeDVZtM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-pvBp6MLm.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DWcwF9y3.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
