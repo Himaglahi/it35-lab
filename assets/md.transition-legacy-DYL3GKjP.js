62278375983ceb03cfcb809a6fecfd6dc4126777
Himaglahi <mjobsidmagsalos@gmail.com>
2025-04-10 08:35:05 +0800

    Updates

--- assets/md.transition-legacy-DYL3GKjP.js
+++ assets/md.transition-legacy-DYL3GKjP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DWcwF9y3.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CABY0aRr.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
