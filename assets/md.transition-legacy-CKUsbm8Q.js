57f652406adf11eedac900205da43216baa30e23
Himaglahi <mjobsidmagsalos>
2025-04-21 19:25:26 +0800

    Updates

--- assets/md.transition-legacy-CKUsbm8Q.js
+++ assets/md.transition-legacy-CKUsbm8Q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C5fJ1xEO.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DiMkRIbv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
